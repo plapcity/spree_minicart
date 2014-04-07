0be18c3e9acef12618369d701a372b77e39cf788
Perri Lapidus <plapidus@gmail.com>
2014-04-07 17:17:48 -0400

    undo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Spree::Core::Engine.routes.draw do
-  get '/cart', :to => 'orders#edit', :as => :cart
-  patch '/cart', :to => 'orders#update', :as => :update_cart
-  put '/cart/empty', :to => 'orders#empty', :as => :empty_cart
+  # Add your extension routes here
 end
